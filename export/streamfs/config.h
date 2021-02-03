93213134d74440c2a475c3239c2591cf2a593225
Zoltan Kuscsik <zoltan.kuscsik@solution57.com>
2021-02-03 12:25:47 +0100

    RDK-3076 - make TSB buffers TS size aligned
    Change-Id: I6bd9f559a4e6913f856e6adfd86aad87913615d2

--- export/streamfs/config.h
+++ export/streamfs/config.h
@@ -5,12 +5,16 @@
 #pragma once
 
 #include <array>
+#include <boost/static_assert.hpp>
 
 // Plugin location directory
 #define STREAMFS_PLUGIN_DIRECTORY "plugins/"
+#define TS_PACKAGE_SIZE 188
 
 // Buffer chunk sizes
-#define BUFFER_CHUNK_SIZE 4096 /* 1k chunks */
+#define BUFFER_CHUNK_SIZE (8 * 6 * TS_PACKAGE_SIZE) /* 1k chunks */
+
+BOOST_STATIC_ASSERT( BUFFER_CHUNK_SIZE % TS_PACKAGE_SIZE == 0);
 
 // Max number of buffer chunks in a list
 #define MAX_BUFFER_LIST_COUNT 256
