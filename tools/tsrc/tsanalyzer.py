2bd0db8c3ab7eecb1e61e65ea2d00cf78169aee0
Christo Joseph <christo.j@tataelxsi.co.in>
2021-11-08 15:49:56 +0530

    RDK-5793 : Optimise streamfs streamanalyser tool to reduce CPU usage
    Add a simple high performance TS parser in TS analyser
    
    Change-Id: I8d2649fcf0961e6cefdb117521250ed7a190a46d

--- tools/tsrc/tsanalyzer.py
+++ tools/tsrc/tsanalyzer.py
@@ -1,78 +1,78 @@
 
 # Transport stream analyzer
-# Author: Zoltan Kuscsik <zoltan@zkres.com>
+# Author: Christo Joseph <christo.j@tataelxsi.co.in>
 
-from abc import abstractmethod
-import subprocess
-from errorlistener import DVBSNOOP_SEPARATOR, ErrorListener
-
+from errorlistener import ErrorListener
 from tslistener import TSListener
-import threading
-import signal
-from multiprocessing import  Lock
-import os
 
+MPEG_TS_PACKET_SIZE=188
+MPEG_TS_NULL_PACKET_PID=8191
 
-class TSAnalyzer(TSListener, threading.Thread):
-    stop_event = threading.Event()
-    mutex = Lock()
-
+class TSAnalyzer(TSListener):
+    residue_buffer = bytearray(0)
+    continuity_map = {}
     def __init__(self, listener : ErrorListener) -> None:
         '''
         @param listener - error Listener
         '''
         super().__init__()
         self.listener = listener
-        self.process = subprocess.Popen(['dvbsnoop', '-s', 'ts', '-nph', '0x100',  '-if', '-'],
-                     stdout = subprocess.PIPE, 
-                     stderr = subprocess.STDOUT,
-                     stdin = subprocess.PIPE)  
-        
-        self.start()
 
     def close(self):
         '''
         Close listener
         '''
-        self.stop_event.set()
-        self.process.terminate()
-        self.process.kill()
 
-    def run(self):
-        running = self.process.poll() is None 
-        packet = ""
+    def check_discontinuity(self, data : bytes, packet_idx : int):
+            pid = ((data[packet_idx+1] << 8) | data[packet_idx+2]) & 0x1FFF
+            current_cc = data[packet_idx+3] & 0x0F
+            if(pid != MPEG_TS_NULL_PACKET_PID):
+                payload_present = data[packet_idx+3] & 0x10
+                if (payload_present):
+                    #print("Pid: ", pid, " Continuity counter : ", current_cc)
+                    prev_cc = self.continuity_map.get(pid)
+                    if (prev_cc != None):
+                        expected_cc = (prev_cc +1) & 0x0F
+                        if (expected_cc != current_cc):
+                            packet = data[packet_idx:packet_idx+188]
+                            print("discontinuity detected of Pid: ", pid, " Continuity counter : ", current_cc, " expected cc: ", expected_cc)
+                            self.listener.on_error_detected(self.listener.error_list['continuity error'], packet.hex())
+                            self.continuity_map.clear()
+                    else:
+                        print("First packet of Pid: ", pid, " Continuity counter : ", current_cc)
+                    self.continuity_map[pid] = current_cc
 
-        print_next_packet = False
-        next_packet_error = None
-        while ( not self.stop_event.is_set()):
-
-            with self.mutex:
-                for line in iter(self.process.stdout.readline, b''):
-                    data = line.decode()
-                    if ( DVBSNOOP_SEPARATOR in data):
-                        if (print_next_packet):
-                            self.listener.on_error_detected(next_packet_error, packet)
-                            print_next_packet = False
-                            next_packet_error = None
-                        packet = ""
-
-                    # Match errors
-                    for k in self.listener.error_list.keys():
-                        if (k in data):
-                            print_next_packet = True
-                            next_packet_error = self.listener.error_list[k]
-                    
-                    packet += data
-
-                    if (self.stop_event.is_set()):
-                        return
-        
     def new_ts_packet(self, data : bytes):
         '''
         New ts packet received
         '''
-        try:
-            if (self.process.poll() is None):
-                self.process.stdin.write(data)
-        except Exception:
-            pass
+        residue_buffer_len = len(self.residue_buffer)
+        packet_idx = 0
+        if (residue_buffer_len):
+            if (self.residue_buffer[0] != 0x47):
+                print("residue buffer : wrong start code : ", hex(self.residue_buffer[0]))
+            else:
+                remaining = MPEG_TS_PACKET_SIZE - residue_buffer_len
+                self.residue_buffer += data[:remaining]
+                self.check_discontinuity(self.residue_buffer, 0)
+                packet_idx = remaining
+        size = len(data)
+        packet_sync_error  = False
+        while (packet_idx <= (size - MPEG_TS_PACKET_SIZE)):
+            if (data[packet_idx] != 0x47):
+                if (False == packet_sync_error):
+                    packet = data[packet_idx:packet_idx+MPEG_TS_PACKET_SIZE]
+                    print("Error : missing sync bytes packet[0]: ", hex(packet[0]), " idx : ", packet_idx)
+                    packet_sync_error = True
+                    self.listener.on_error_detected(self.listener.error_list['continuity error'], packet.hex())
+                packet_idx += 1
+                continue
+            if (packet_sync_error):
+                print("Sync restored, idx : ", packet_idx)
+                packet_sync_error = False
+            self.check_discontinuity(data, packet_idx)
+            packet_idx += MPEG_TS_PACKET_SIZE
+        if (packet_idx < size):
+            self.residue_buffer = bytearray(data[packet_idx:])
+        else:
+            self.residue_buffer = bytearray(0)
